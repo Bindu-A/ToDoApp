866f94058077ba44eb4c3c0f10d102d3ee9ae75e
Bindu-A <binduakalale@gmail.com>
2022-10-12 12:13:14 +0530

    Add authentication

--- src/main/java/com/example/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/example/myfirstwebapp/login/LoginController.java
@@ -1,7 +1,6 @@
 package com.example.myfirstwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +10,26 @@ import org.springframework.web.bind.annotation.*;
 public class LoginController {
 
     ///login => com.in28minutes.springboot.myfirstwebapp.login.LoginController => login.jsp
-    private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @GetMapping("login/{name}")
-    public String gotoLoginPage(@PathVariable String name, ModelMap modelMap) {
-        logger.debug("Path variable {}",name);
-        modelMap.put("name", name);
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @GetMapping("login")
+    public String gotoLoginPage() {
+        return "login";
+    }
+
+
+    @PostMapping(value="login")
+    public String gotoWelcomePage(@RequestParam String name,
+                                  @RequestParam String password, ModelMap model) {
+
+        if(authenticationService.authenticate(name, password)) {
+
+            model.put("name", name);
+            return "welcome";
+        }
+        model.put("errormessage","Invalid credential");
         return "login";
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -4,7 +4,11 @@
 	</head>
 	<body>
 		Welcome to the login page!
-
-		${name}
+	    <pre>${errormessage}</pre>
+		<form method="post">
+		   Name: <input type="text" name="name">
+           Password: <input type="password" name="password">
+           <input type="submit">
+		</form>
 	</body>
 </html>
