06068f06d1477589e21f09d6924cc2a28f09e68e
Bindu-A <binduakalale@gmail.com>
2022-10-22 10:54:47 +0530

    Show TodoList with new todo added

--- src/main/java/com/example/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/example/myfirstwebapp/todo/TodoController.java
@@ -22,14 +22,17 @@ public class TodoController {
     }
 
     @GetMapping("/add-todo")
-    public String showNewTodoPage(){
+    public String showNewTodoPage(ModelMap model) {
+        String username = (String) model.get("name");
+        Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+        model.put("todo",todo);
         return "todo";
     }
 
     @PostMapping("/add-todo")
-    public String addNewTodo(@RequestParam String description, ModelMap model){
-        String username = (String)model.get("name");
-        todoService.addTodo(username, description,
+    public String addNewTodo(ModelMap model,Todo todo) {
+        String username = (String) model.get("name");
+        todoService.addTodo(username, todo.getDescription(),
                 LocalDate.now().plusYears(1), false);
         return "redirect:list-todos";
     }
