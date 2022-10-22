ab98de9308bb15d8e96adecdf19285f0b8fef645
Bindu-A <binduakalale@gmail.com>
2022-10-22 11:33:10 +0530

    Add Validation to description

--- src/main/java/com/example/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/example/myfirstwebapp/todo/TodoController.java
@@ -1,8 +1,10 @@
 package com.example.myfirstwebapp.todo;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -25,12 +27,15 @@ public class TodoController {
     public String showNewTodoPage(ModelMap model) {
         String username = (String) model.get("name");
         Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
-        model.put("todo",todo);
+        model.put("todo", todo);
         return "todo";
     }
 
     @PostMapping("/add-todo")
-    public String addNewTodo(ModelMap model,Todo todo) {
+    public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+        if (result.hasErrors()) {
+            return "todo";
+        }
         String username = (String) model.get("name");
         todoService.addTodo(username, todo.getDescription(),
                 LocalDate.now().plusYears(1), false);
