7ead0087891e18ec357e7ff400e7043e3cc32ec9
Bindu-A <binduakalale@gmail.com>
2022-10-27 17:00:08 +0530

    Remove Login logic

---
+++ src/main/java/com/example/myfirstwebapp/login/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.example.myfirstwebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@SessionAttributes("name")
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String gotoWelcomePage(ModelMap model) {
+        model.put("name", "bindu");
+        return "welcome";
+    }
+
+}
